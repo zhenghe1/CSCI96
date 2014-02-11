4771fef89a7b1425f73830ea742610a01312539b
Zheng He <zhenghe12613@gmail.com>
2014-02-10 20:50:46 -0500

    Fixed some bugs. MemoryManager file became corrupted for some reason...

--- proj1/MemoryManager.hpp
+++ proj1/MemoryManager.hpp
@@ -1,22 +1,29 @@
 #ifndef MEMORYMANAGER_HPP
 #define MEMORYMANAGER_HPP
 
-/*
- * These classes are for memory management via a free list. Implementation is very similar to the one on IBM's developer tutorial guides. 
- * Methods to allocate and free nodes for the tree class.
- * Does so in a way that it limits the number of context switches between the user and the kernel.
- */
+#include <stdlib.h>
+#include <iostream>
 
 class IMemoryManager {
     public:
         virtual void *allocate(size_t) = 0;
-        virtual void free(voide *) = 0;
-}
+        virtual void free(void *) = 0;
+};
 
 class MemoryManager : public IMemoryManager {
+    private:
+        struct FreeList {
+            FreeList *next;
+        };
+
+        FreeList *freeListHead;
+
+        void getMoreNodes(size_t, size_t);
+        void cleanUp();
+
     public:
-        MemoryManager() : freeListHead(0) {
-            getMoreNodes();
+        MemoryManager(size_t size) : freeListHead(0) {
+            getMoreNodes(256, size);
         }
         virtual ~MemoryManager() {
             cleanUp();
@@ -24,41 +31,28 @@ class MemoryManager : public IMemoryManager {
         virtual void *allocate(size_t);
         virtual void free(void *);
 
-    private:
-        struct FreeList {
-            FreeList *next;
-        }
-        
-        FreeList *freeListHead;
+};
 
-        void getMoreNodes(size_t, size_t);
-        void cleanUp();
-
-}
-
-// Returns a node from the free list
-inline void *MemoryManager::allocate(size_t size) {
-    if(freeListHead == 0) getMoreNodes(256, size); 
-    
+inline void *MemoryManager::allocate(size_t size = 24) {
+    if (freeListHead == 0) getMoreNodes(256, size);
     FreeList *head = freeListHead;
     freeListHead = head->next;
+
     return head;
 }
 
-// Adds an unused node back to the free list
-inline void MemoryManager:: free(void *deletedNode) {
+inline void MemoryManager::free(void *deletedNode) {
     FreeList *head = static_cast<FreeList *>(deletedNode);
     head->next = freeListHead;
     freeListHead = head;
 }
 
-// Allocates a chunk of memory to be used as nodes as part of the free list
 inline void MemoryManager::getMoreNodes(size_t poolSize, size_t objectSize) {
     size_t size = objectSize > sizeof(FreeList *) ? objectSize : sizeof(FreeList *);
     FreeList *head = reinterpret_cast<FreeList *>(new char[size]);
     freeListHead = head;
 
-    for(int i = 0; i < poolSize; ++i) {
+    for(int i = 0; i < poolSize; i++) {
         head->next = reinterpret_cast<FreeList *>(new char[size]);
         head = head->next;
     }
@@ -66,7 +60,6 @@ inline void MemoryManager::getMoreNodes(size_t poolSize, size_t objectSize) {
     head->next = 0;
 }
 
-// De-allocates the free list
 inline void MemoryManager::cleanUp() {
     FreeList *nextPtr = freeListHead;
     for(; nextPtr; nextPtr = freeListHead) {
